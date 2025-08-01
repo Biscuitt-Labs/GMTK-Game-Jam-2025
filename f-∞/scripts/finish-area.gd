0f19f9ea3bcfa9879916f696099847731b9f2c9a
NoahfenseM8 <nalichlyter@gmail.com>
2025-08-01 12:57:03 -0400

    commented out unused elif statements in collider scripts

--- f-∞/scripts/finish-area.gd
+++ f-∞/scripts/finish-area.gd
@@ -3,12 +3,12 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "test-vehicle-a":
 		print("player entered finish area")
-	elif body is RigidBody3D:
-		print ("a rigidbody3d entered finish area: ", body.name)
+	#elif body is RigidBody3D: #for checking non-player objects
+	#	print ("a rigidbody3d entered finish area: ", body.name)
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if body.name == "test-vehicle-a":
 		print("player exited finish area")
-	elif body is RigidBody3D:
-		print ("a rigidbody3d exited finish area: ", body.name)
+	# elif body is RigidBody3D: #for checking non-player objects
+	#	print ("a rigidbody3d exited finish area: ", body.name)
--- f-∞/scripts/start-area.gd
+++ f-∞/scripts/start-area.gd
@@ -3,12 +3,12 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "test-vehicle-a":
 		print("player entered start area")
-	elif body is RigidBody3D:
-		print ("a rigidbody3d entered start area: ", body.name)
+	# elif body is RigidBody3D: #for checking non-player objects
+	# 	print ("a rigidbody3d entered start area: ", body.name)
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if body.name == "test-vehicle-a":
 		print("player exited start area")
-	elif body is RigidBody3D:
-		print ("a rigidbody3d exited start area: ", body.name)
+	# elif body is RigidBody3D: #for checking non-player objects
+	# 	print ("a rigidbody3d exited start area: ", body.name)
