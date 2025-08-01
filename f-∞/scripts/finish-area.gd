14d351072eebd9bc29e2c58f9d32bdb24bf19c21
NoahfenseM8 <nalichlyter@gmail.com>
2025-08-01 17:30:27 -0400

    passing finish line now teleports player back to spawn point

--- f-∞/scripts/finish-area.gd
+++ f-∞/scripts/finish-area.gd
@@ -1,12 +1,15 @@
 extends Area3D
 
-
+@export var spawn_area: Node3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "test-vehicle-a":
 		print("player entered finish area")
-	#elif body is RigidBody3D: #for checking non-player objects
-	#	print ("a rigidbody3d entered finish area: ", body.name)
+	elif body is RigidBody3D: #for checking non-player objects
+		print ("a rigidbody3d entered finish area: ", body.name)
+	
+	body.position = spawn_area.get_marker_position()
+	print("moved body back to spawn point")
 
 
 func _on_body_exited(body: Node3D) -> void:
